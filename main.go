ed08951fe7719a105074011db96585d1d670a5f2
Taras Postument <trane9991@gmail.com>
2016-09-11 10:11:03 +0300

    added external dependency

--- main.go
+++ main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github.com/kelseyhightower/envconfig"
 	"html"
 	"log"
 	"net/http"
 )
 
+type configuration struct {
+	Listen string `default:"9090" envconfig:"http_platform_port"`
+}
+
 func main() {
+	var cnf configuration
+	err := envconfig.Process("", &cnf)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		str := fmt.Sprintf("Hello, %q", html.EscapeString(r.URL.Path))
 		fmt.Fprintf(w, str)
@@ -18,7 +29,6 @@ func main() {
 		fmt.Fprintf(w, "Hello, Golang Meetup!")
 		log.Println("Hello, Golang Meetup")
 	})
-
-	log.Fatal(http.ListenAndServe(":8081", nil))
-
+	log.Printf("Starting web server on port %s", cnf.Listen)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", cnf.Listen), nil))
 }
