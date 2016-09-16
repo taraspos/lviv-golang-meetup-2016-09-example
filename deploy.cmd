55ad15118594515fcb16faf12614b08fa55c991b
Taras Postument <trane9991@gmail.com>
2016-09-17 00:00:38 +0300

    remove func to modify path

--- deploy.cmd
+++ deploy.cmd
@@ -93,7 +93,7 @@ SET GOPATH=%DEPLOYMENT_TEMP%\gopath
 SET GOEXE="%GOROOT%\bin\go.exe"
 SET FOLDERNAME=azureapp
 SET GOAZUREAPP=%DEPLOYMENT_TEMP%\gopath\src\%FOLDERNAME%
-$env:Path += ";%GOROOT%"
+$env:Path += ";%GOROOT%\bin"
 
 IF NOT EXIST %GOEXE% (
   ECHO go.exe not found!
@@ -129,53 +129,6 @@ IF NOT EXIST %GLIDEEXE% (
   ECHO glide.exe not found!
   goto error
 )
-
-function Add-Path {
-  <#
-    .SYNOPSIS
-      Adds a Directory to the Current Path
-    .DESCRIPTION
-      Add a directory to the current path.  This is useful for 
-      temporary changes to the path or, when run from your 
-      profile, for adjusting the path within your powershell 
-      prompt.
-    .EXAMPLE
-      Add-Path -Directory "C:\Program Files\Notepad++"
-    .PARAMETER Directory
-      The name of the directory to add to the current path.
-  #>
-
-  [CmdletBinding()]
-  param (
-    [Parameter(
-      Mandatory=$True,
-      ValueFromPipeline=$True,
-      ValueFromPipelineByPropertyName=$True,
-      HelpMessage='What directory would you like to add?')]
-    [Alias('dir')]
-    [string[]]$Directory
-  )
-
-  PROCESS {
-    $Path = $env:PATH.Split(';')
-
-    foreach ($dir in $Directory) {
-      if ($Path -contains $dir) {
-        Write-Verbose "$dir is already present in PATH"
-      } else {
-        if (-not (Test-Path $dir)) {
-          Write-Verbose "$dir does not exist in the filesystem"
-        } else {
-          $Path += $dir
-        }
-      }
-    }
-
-    $env:PATH = [String]::Join(';', $Path)
-  }
-}
-
-Add-Path -Directory "%GOROOT%\bin"
 
 %GLIDEEXE% install
 
